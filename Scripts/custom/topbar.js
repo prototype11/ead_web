dc441d3b9944e7b37af92f4c9aa52cf6e7cb42c9
Khawaja Jawad Shuja <jays.prototype11@gmail.com>
2015-02-10 21:12:23 +0500

    First Commit!
    Adding MVC project.

---
+++ Scripts/custom/topbar.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $("#signup-form-ead").validate({
+        errorPlacement: function (error, element) {
+            element.parent().after(error);
+        },
+        rules: {
+            Email: {
+                required: true,
+                email:true
+            }
+        }
+    });
+    $("#login-form-ead").validate({
+        errorPlacement: function (error, element) {
+            element.parent().after(error);
+        }
+    });
+
+    $('#btn-verify-name-ead').click(function () {
+        var userName = $('#signup-name-ead').val();
+        $.getJSON("/User/UsernameExists?Username=" + userName, function (data) {
+            if (data) {
+                $('#verify-result-ead').text("User already exists in database");
+            }
+            else {
+                $('#verify-result-ead').text("User Name is available");
+            }
+        });
+    });
+});
